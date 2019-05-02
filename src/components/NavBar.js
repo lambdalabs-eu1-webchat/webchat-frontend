f2e182d4e8ca6c411898764117fe1917d788325c
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-05-02 16:25:28 +0100

    Add conditional operator for rendering the nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,13 +2,24 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-const NavBar = () => (
+const NavBar = ({ loggedIn }) => (
     <div>
-      <nav>
-        <NavLink to='/register'>Register</NavLink>
-        <NavLink to='/login'>Login</NavLink>
-      </nav>
+      {!loggedIn ? (
+          <nav>
+            <NavLink to='/register'>Register</NavLink>
+            <NavLink to='/login'>Login</NavLink>
+          </nav>
+      ) : (
+          <nav>
+            <NavLink to='/logout'>Logout</NavLink>
+          </nav>
+      )}
+
     </div>
 );
+
+NavBar.propTypes = {
+  loggedIn: PropTypes.bool.isRequired,
+};
 
 export default NavBar;
