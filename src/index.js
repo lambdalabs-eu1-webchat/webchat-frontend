71f4f40f8f89cc5ba1fad6c41332f0c6b1c336c7
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-05-02 14:59:35 +0100

    Add Router

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import rootReducer from './store/reducers/index';
 import thunk from 'redux-thunk';
 
@@ -18,7 +19,9 @@ const store = createStore(
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>, document.getElementById('root'),
 );
 
