6b41bd6388daa722e198687f62ec03db489f84fe
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-05-06 23:13:01 +0100

    Create TeamMember component

---
+++ src/components/TeamMember.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const TeamMember = (props) => {
+  const { name, email, user_type, currentUser } = props;
+  if(user_type === 'admin' || user_type === 'super admin' && currentUser.user_type === 'super admin') {
+    return (
+        <div className="hotel-staff">
+          <p>{ name }</p>
+          <p>{ email }</p>
+          <input type="checkbox" defaultChecked/>
+          <p><i className="fas fa-trash-alt" onClick={console.log('delete member')}></i></p>
+        </div>
+    );
+  }
+  return (
+      <div className="hotel-staff">
+        <p>{ name }</p>
+        <p>{ email }</p>
+        <input type="checkbox" disabled="disabled" readOnly/>
+        <p><i className="fas fa-trash-alt"></i></p>
+      </div>
+  )
+};
+
+export default TeamMember;
