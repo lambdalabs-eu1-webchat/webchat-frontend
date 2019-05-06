436926f7a9f9abed4878a22fa251c59bd82144c7
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-05-06 23:10:13 +0100

    Add currentUser to combineReducers

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from "redux";
 import users from './users';
 import authToken from './auth';
+import currentUser from './currentUser';
 
 export default combineReducers({
-  users, authToken,
+  users, authToken, currentUser
 });
 
 
