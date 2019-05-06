bd9a81cfb4cc338492935763c8a009d159fcfcab
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-05-06 23:13:21 +0100

    Create TeamMemberList component

---
+++ src/components/TeamMembersList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import TeamMember from './TeamMember';
+
+const TeamMembersList = (props) => {
+  return (
+      <div className="team-members-list">
+        <div className="list-header">
+          <p>Name</p>
+          <p>Email</p>
+          <p>Admin</p>
+          <p>Remove</p>
+        </div>
+        {props.users.map(user => (
+            <TeamMember key={`${user.id} + ${user.name}`} name={user.name} email={user.email} currentUser={props.currentUser} user_type={user.user_type}/>
+        ))}
+      </div>
+  )
+};
+
+export default TeamMembersList;
