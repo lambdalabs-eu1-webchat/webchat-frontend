719126e07b362fc2d15a2e909b37757337c1c796
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-05-07 21:56:23 +0100

    Update TeamMembersList component

--- src/components/TeamMembersList.js
+++ src/components/TeamMembersList.js
@@ -2,23 +2,27 @@ import React from 'react';
 import TeamMember from './TeamMember';
 
 const TeamMembersList = (props) => {
+  const { deleteUser, changeUserType } = props;
   return (
-      <div className="team-members-list">
-        <div className="list-header">
-          <p>Name</p>
-          <p>Email</p>
-          <p>Admin</p>
-          <p>Remove</p>
-        </div>
-        {props.users.map(user => (
-            <TeamMember
-                key={`${user.id} + ${user.name}`}
-                name={user.name} email={user.email}
-                currentUser={props.currentUser}
-                user_type={user.user_type}
-            />
-        ))}
+    <div className="team-members-list">
+      <div className="list-header">
+        <p>Name</p>
+        <p>Email</p>
+        <p>Admin</p>
+        <p>Remove</p>
       </div>
+      {props.users.map(user => (
+        <TeamMember
+          key={`${user._id} + ${user.name}`}
+          name={user.name} email={user.email}
+          currentUser={props.currentUser}
+          user_type={user.user_type}
+          userId={user._id}
+          changeUserType={changeUserType}
+          deleteUser={deleteUser}
+        />
+      ))}
+    </div>
   )
 };
 
