32a487168e1cd37d44f19d7404d8e13846c72379
Talent Antonio <antonio.talenty@gmail.com>
2019-05-07 14:29:04 +0100

    Dashboard /any ones dash

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,1 +1,12 @@
-impo
+import React, { Component } from 'react'
+
+class Dashboard extends Component {
+    render(){
+        <div className="dashoard container">
+        <div className="row">
+        <div className="col s12 m6"></div>
+        <div className="col s12 m5 offset-m1"></div>
+        </div>
+     </div>
+    }
+}
