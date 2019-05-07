c368307bff6928330075c6d4f9111c0856aeec7d
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-05-07 21:58:24 +0100

    Complete promote to admin functionality

--- src/components/TeamMember.js
+++ src/components/TeamMember.js
@@ -1,25 +1,36 @@
 import React from 'react';
 
-const TeamMember = (props) => {
-  const { name, email, user_type, currentUser } = props;
-  if(user_type === 'admin' || user_type === 'super admin' && currentUser.user_type === 'super admin') {
-    return (
-        <div className="hotel-staff">
-          <p>{ name }</p>
-          <p>{ email }</p>
-          <input type="checkbox" defaultChecked/>
-          <p><i className="fas fa-trash-alt"></i></p>
-        </div>
-    );
+const handleAdminPromotion = (changeUserType, id, currentUser, user_type) => {
+  if (currentUser.user_type !== 'super admin') {
+    return () => {};
   }
+  return (event) => {
+    if (user_type === 'super admin') {
+      event.preventDefault();
+      return;
+    }
+    if (!event.target.checked) {
+      changeUserType(id, 'receptionist');
+    } else {
+      changeUserType(id, 'admin');
+    }
+  };
+};
+  }
+const TeamMember = ({ name, email, user_type, currentUser, changeUserType, userId, deleteUser }) => {
+  const isAdmin = user_type === 'admin' || user_type === 'super admin';
   return (
-      <div className="hotel-staff">
-        <p>{ name }</p>
-        <p>{ email }</p>
-        <input type="checkbox" disabled="disabled" readOnly/>
-        <p><i className="fas fa-trash-alt"></i></p>
-      </div>
-  )
+    <div className="hotel-staff">
+      <p>{ name }</p>
+      <p>{ email }</p>
+      <input
+        type="checkbox"
+        checked={isAdmin}
+        disabled={user_type === 'super admin'}
+        onChange={handleAdminPromotion(changeUserType, userId, currentUser, user_type)}
+      />
+    </div>
+  );
 };
 
 export default TeamMember;
