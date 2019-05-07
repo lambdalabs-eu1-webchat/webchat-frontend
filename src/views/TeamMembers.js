9f89f8eb52ccc78123c015a2b67bc08fc95ce33e
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-05-07 21:55:50 +0100

    Pass in props, update proptypes & mapStateToProps & required imports

--- src/views/TeamMembers.js
+++ src/views/TeamMembers.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from "react-redux";
-import { fetchAllUsers, fetchHotelStaff, createUser } from "../store/actions/users";
+import { fetchAllUsers, fetchHotelStaff, createUser, changeUserType, deleteUser } from "../store/actions/users";
 
 import SuperAdminNav from '../components/SuperAdminNav';
 import TeamMembersList from '../components/TeamMembersList';
@@ -26,20 +27,34 @@ class TeamMembers extends React.Component {
   };
 
   render() {
-    const { state, dispatchCreateUser } = this.props;
-
+    const { state, dispatchCreateUser, dispatchChangeUserType, dispatchDeleteUser } = this.props;
     return (
-        <div className="team-members">
-          <SuperAdminNav/>
-          <h2>Team Members Page</h2>
-          <h3>Update and Assign Team Members</h3>
-          <TeamMembersList users={state.users} currentUser={state.currentUser}/>
-          <TeamMembersAddNewMemberModal createUser={dispatchCreateUser} handleHideModal={this.handleHideModal} modalShown={this.state.modalShown}/>
-          <button onClick={this.handleShowModal}>Add Team Members</button>
-        </div>
+      <div className="team-members">
+        <SuperAdminNav/>
+        <h2>Team Members Page</h2>
+        <h3>Update and Assign Team Members</h3>
+        <TeamMembersList
+          users={state.users}
+          currentUser={state.currentUser}
+          changeUserType={dispatchChangeUserType}
+          deleteUser={dispatchDeleteUser}
+        />
+        <TeamMembersAddNewMemberModal
+          createUser={dispatchCreateUser}
+          handleHideModal={this.handleHideModal}
+          modalShown={this.state.modalShown}
+        />
+        <button onClick={this.handleShowModal}>Add Team Members</button>
+      </div>
     );
   };
 }
+
+TeamMembers.propTypes = {
+  state: PropTypes.shape().isRequired,
+  dispatchChangeUserType: PropTypes.func.isRequired,
+  dispatchDeleteUser: PropTypes.func.isRequired,
+};
 
 const mapStateToProps = state => ({ state });
 
@@ -47,4 +62,6 @@ export default connect(mapStateToProps, {
   dispatchFetchAllUsers: fetchAllUsers,
   dispatchFetchHotelStaff: fetchHotelStaff,
   dispatchCreateUser: createUser,
+  dispatchChangeUserType: changeUserType,
+  dispatchDeleteUser: deleteUser,
 })(TeamMembers);
