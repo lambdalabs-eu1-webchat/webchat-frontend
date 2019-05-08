456d2ceb080c160a99ff0dc91687e1b337b7cd5e
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-05-08 11:08:00 +0100

    Create action creators for hotel & add hotel path

---
+++ src/store/actions/hotel.js
@@ -0,0 +1,103 @@
+import { DOMAIN, HOTEL } from '../../utils/paths';
+import {
+  FETCH_SINGLE_HOTEL_SUCCESS,
+  FETCH_SINGLE_HOTEL_FAILURE,
+  FETCH_SINGLE_HOTEL,
+  UPDATE_HOTEL_SUCCESS,
+  UPDATE_HOTEL_FAILURE,
+  UPDATE_HOTEL,
+} from './actionTypes';
+
+// Synchronous action creators
+
+export const fetchSingleHotelSuccess = hotel => {
+  if (!hotel) {
+    throw new Error('fetchSingleHotelSuccess requires a hotel argument');
+  }
+  return {
+    type: FETCH_SINGLE_HOTEL_SUCCESS,
+    payload: {
+      hotel,
+    },
+  };
+};
+
+export const fetchSingleHotelFailure = error => {
+  if (!error) {
+    throw new Error('fetchSingleHotelFailure requires an error argument');
+  }
+  return {
+    type: FETCH_SINGLE_HOTEL_FAILURE,
+    payload: {
+      error,
+    },
+  };
+};
+
+export const updateHotelSuccess = updatedHotel => {
+  if (!updatedHotel) {
+    throw new Error('updateHotelSuccess requires an updatedHotel argument');
+  }
+  return {
+    type: UPDATE_HOTEL_SUCCESS,
+    payload: {
+      updatedHotel,
+    },
+  };
+};
+
+export const updateHotelFailure = error => {
+  if (!error) {
+    throw new Error('updateHotelFailure requires an error argument');
+  }
+  return {
+    type: UPDATE_HOTEL_FAILURE,
+    payload: {
+      error,
+    },
+  };
+};
+
+// Asynchronous action creators
+
+export const fetchSingleHotel = id => async dispatch => {
+  dispatch({ type: FETCH_SINGLE_HOTEL });
+  try {
+    const result = await fetch(`${DOMAIN}${HOTEL}/${id}`);
+    const jsonResult = await result.json();
+    dispatch(fetchSingleHotelSuccess(jsonResult));
+  } catch (error) {
+    dispatch(fetchSingleHotelFailure(error));
+  }
+};
+
+export const updateHotel = (
+    id,
+    name,
+    motto,
+) => async dispatch => {
+  dispatch({ type: UPDATE_HOTEL });
+  const updatedHotel = {
+    name: String(name),
+    motto: String(motto)
+  };
+  const config = {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(updatedHotel),
+  };
+  try {
+    const result = await fetch(`${DOMAIN}${HOTEL}/${id}`, config);
+    const jsonResult = await result.json();
+    if (result.ok) {
+      const newHotel = { ...jsonResult };
+      dispatch(updateHotelSuccess(newHotel));
+    } else {
+      throw new Error(jsonResult.message);
+    }
+  } catch (error) {
+    dispatch(updateHotelFailure(error.message));
+  }
+};
--- src/utils/paths.js
+++ src/utils/paths.js
@@ -2,6 +2,7 @@ export const DOMAIN = 'http://localhost:7000/';
 export const LOGIN = 'api/auth/login';
 export const REGISTER = 'api/auth/register';
 export const USERS = 'api/users';
+export const HOTEL = 'api/hotel';
 
 export const SOCKET = {
   CLOSE_TICKET: 'close_ticket',
