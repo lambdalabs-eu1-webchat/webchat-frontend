7c053acc166e5c4e0596e4bf6455de7143f4b898
Pav0l <pavol.karas@gmail.com>
2019-05-08 14:18:17 +0200

    Add tickets status in a separate utils file

--- src/views/Chat.js
+++ src/views/Chat.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
+
 import ChatsList from '../components/ChatsList';
+import { QUEUED, ACTIVE, CLOSED } from '../utils/ticketStatus';
 
 class Chat extends React.Component {
   render() {
     return (
       <div>
         <h2>Welcome to the Chat page!</h2>
-        <ChatsList chatsArr={this.props.queuedChats} status="queued" />
-        <ChatsList chatsArr={this.props.activeChats} status="active" />
+        <ChatsList chatsArr={this.props.queuedChats} status={QUEUED} />
+        <ChatsList chatsArr={this.props.activeChats} status={ACTIVE} />
       </div>
     );
   }
