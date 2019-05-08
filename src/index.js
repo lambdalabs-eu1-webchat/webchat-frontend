b439fe7aacacdeca4b40a078724840447763c7f6
conconrob <connortprobertson@gmail.com>
2019-05-08 12:59:46 +0100

    merge master

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const store = createStore(
   compose(
     applyMiddleware(thunk),
     // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  ),
 );
 
 ReactDOM.render(
@@ -22,6 +22,6 @@ ReactDOM.render(
     <Router>
       <App />
     </Router>
-  </Provider>, document.getElementById('root'),
+  </Provider>,
+  document.getElementById('root'),
 );
-
