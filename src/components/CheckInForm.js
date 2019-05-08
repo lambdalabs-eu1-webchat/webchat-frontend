952e18ea58ff1f71dba4cd6d1cbc96214ea4fcd5
conconrob <connortprobertson@gmail.com>
2019-05-08 15:18:09 +0100

    checking is now keeping teck of room

--- src/components/CheckInForm.js
+++ src/components/CheckInForm.js
@@ -1,19 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
 import propTypes from 'prop-types';
+import axios from 'axios';
+import { DOMAIN, USERS } from '../utils/paths';
 
 class CheckInForm extends React.Component {
   state = {
     nameInput: '',
+    loginCode: '',
+    currentRoom: null,
   };
   setNameInput = nameInput => {
     this.setState({ nameInput });
+  };
+  setCurrentRoom = event => {
+    const room = this.props.rooms.find(room => room._id === event.target.value);
+    this.setState({ currentRoom: room });
+  };
+
+  checkInGuest = () => {
+    axios.post(`${DOMAIN}${USERS}`); //{hotel_id,user_type,name:this.state.nameInput,room}
   };
 
   render() {
     return (
       <StyledCheckInForm>
-        <select>
+        <select defaultValue={''} onChange={this.setCurrentRoom}>
+          <option value='' disabled>
+            Select your option
+          </option>
           {this.props.rooms.map(room => (
             <option key={room._id} value={room._id}>
               {room.name}
@@ -25,6 +40,10 @@ class CheckInForm extends React.Component {
           onChange={event => this.setNameInput(event.target.value)}
         />
         <button>Check In</button>
+        <div>
+          <h4>Login Code</h4>
+          <p>{this.state.loginCode}</p>
+        </div>
       </StyledCheckInForm>
     );
   }
