70555d01acb6649f53bc04751b9a16c5454cf768
conconrob <connortprobertson@gmail.com>
2019-05-08 14:25:04 +0100

    added room reducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -3,10 +3,12 @@ import users from './users';
 import authToken from './auth';
 import currentUser from './currentUser';
 import chats from './chats';
+import rooms from './rooms';
 
 export default combineReducers({
   users,
   authToken,
   chats,
   currentUser,
+  rooms,
 });
