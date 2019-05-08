4aaca1839f6097c18047068014162c757afc714d
conconrob <connortprobertson@gmail.com>
2019-05-08 14:36:52 +0100

    pulling in hotel rooms

--- src/views/CheckInOrOut.js
+++ src/views/CheckInOrOut.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 import propTypes from 'prop-types';
-
+import { connect } from 'react-redux';
+import { fetchRooms } from '../store/actions/rooms';
 class CheckInOrOut extends React.Component {
   componentDidMount() {
     // get the rooms
+    this.props.fetchRooms(this.props.hotel_id);
+    console.log(this.props.hotel_id);
   }
   render() {
     return <StyledCheckInOrOut>fdfdf</StyledCheckInOrOut>;
@@ -15,4 +18,16 @@ CheckInOrOut.propTypes = {};
 
 const StyledCheckInOrOut = styled.div``;
 
-export default CheckInOrOut;
+function mstp(state) {
+  return {
+    rooms: state.rooms.rooms,
+    fetchingRooms: state.rooms.fetching,
+    roomsError: state.rooms.error,
+    hotel_id: state.currentUser.hotel_id,
+  };
+}
+
+export default connect(
+  mstp,
+  { fetchRooms },
+)(CheckInOrOut);
