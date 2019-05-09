b497f2087abfb568f818576138f8d9881b80a743
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-05-09 22:33:10 +0100

    Add functionality to add a single room

--- src/components/CompanySettingsRoomsList.js
+++ src/components/CompanySettingsRoomsList.js
@@ -1,14 +1,54 @@
 import React from 'react';
 import CompanySettingsRoom from "./CompanySettingsRoom";
 
+const handleClick = (
+    createRoomForHotel,
+    id
+) => event => {
+  event.preventDefault();
+  let roomName = '';
+  event.target.parentNode.childNodes.forEach(childNode => {
+    if (childNode.name === 'roomName') {
+      roomName = childNode.value;
+    }
+  });
+  let blank = false;
+  if (roomName ) {
+    createRoomForHotel(roomName, id);
+  } else {
+    blank = true;
+  }
+
+  if (blank) {
+    event.target.parentNode.childNodes.forEach(childNode => {
+      if (childNode.getAttribute('id') === 'add-room-message') {
+        childNode.textContent = 'Please fill in all the required fields.';
+      }
+    });
+  } else {
+    event.target.parentNode.childNodes.forEach(childNode => {
+      if (childNode.name === 'roomName') {
+        childNode.value = '';
+      }
+    });
+  }
+};
+
+
 const CompanySettingsRoomsList = (props) => {
-  const { rooms, deleteRoomForHotel, updateRoomForHotel, currentUser, handleInputChange} = props;
+  const { rooms, deleteRoomForHotel, updateRoomForHotel, currentUser, handleInputChange, createRoomForHotel } = props;
   return (
     <div className="rooms-list">
+      <section className="add-rooms">
+        <input placeholder="Add rooms..." name="roomName"/>
+        <button type="submit" onClick={handleClick(createRoomForHotel, currentUser.hotel_id)}>ADD ROOMS</button>
+      </section>
       {rooms && rooms.map(room => (
         <CompanySettingsRoom
           key={room._id}
           name={room.name}
+          room={room._id}
+          hotelId={currentUser.hotel_id}
           currentUser={currentUser}
           updateRoomForHotel={updateRoomForHotel}
           deleteRoomForHotel={deleteRoomForHotel}
