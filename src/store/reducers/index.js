4dcd2c9d777386e65505694ac466b0d55cdd5c39
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-05-09 12:07:39 +0100

    Remove authToken from combineReducers

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,15 +1,13 @@
 import { combineReducers } from 'redux';
 import users from './users';
-import authToken from './auth';
 import currentUser from './currentUser';
 import chats from './chats';
 import hotel from './hotel';
+import rooms from './rooms';
 
 export default combineReducers({
   users,
-  authToken,
   chats,
   hotel,
   currentUser,
-  hotel,
 });
