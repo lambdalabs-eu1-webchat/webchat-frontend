1a4e5c72e689d8dfdf9ec8857dec92510ce2c992
conconrob <connortprobertson@gmail.com>
2019-05-14 17:15:16 +0100

    made the navlink work

--- src/views/CompanyDash.js
+++ src/views/CompanyDash.js
@@ -1,27 +1,37 @@
 import React from 'react';
 import SuperAdminNav from '../components/SuperAdminNav';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Billing from '../views/Billing';
 import TeamMembers from '../views/TeamMembers';
 import CompanySettings from '../views/CompanySettings';
 import { APP_PATHS } from '../utils/paths';
+import { Redirect } from 'react-router-dom';
 function CompanyDash() {
   return (
     <div>
       <SuperAdminNav />
-
-      <Route
-        path={`${APP_PATHS.COMPANY_DASH}${APP_PATHS.BILLING}`}
-        component={Billing}
-      />
-      <Route
-        path={`${APP_PATHS.COMPANY_DASH}${APP_PATHS.TEAM_MEMBERS}`}
-        component={TeamMembers}
-      />
-      <Route
-        path={`${APP_PATHS.COMPANY_DASH}${APP_PATHS.COMPANY_SETTINGS}`}
-        component={CompanySettings}
-      />
+      <Switch>
+        <Route
+          path={`${APP_PATHS.COMPANY_DASH}${APP_PATHS.BILLING}`}
+          component={Billing}
+        />
+        <Route
+          path={`${APP_PATHS.COMPANY_DASH}${APP_PATHS.TEAM_MEMBERS}`}
+          component={TeamMembers}
+        />
+        <Route
+          path={`${APP_PATHS.COMPANY_DASH}${APP_PATHS.COMPANY_SETTINGS}`}
+          component={CompanySettings}
+        />
+        <Route
+          path={`${APP_PATHS.COMPANY_DASH}`}
+          render={() => (
+            <Redirect
+              to={APP_PATHS.COMPANY_DASH + APP_PATHS.COMPANY_SETTINGS}
+            />
+          )}
+        />
+      </Switch>
     </div>
   );
 }
