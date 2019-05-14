4a5c2676b2c24990c9813bd156aed258cf91a535
Pav0l <pavol.karas@gmail.com>
2019-05-14 17:21:01 +0200

    Console log

--- src/views/Chat.js
+++ src/views/Chat.js
@@ -97,7 +97,6 @@ const mapStateToProps = state => {
       currentChat = chats.closedChats.find(chat => chat._id === chat_id);
     }
   }
-  console.log(state.currentUser);
   return {
     queuedChats: chats.queuedChats,
     activeChats: chats.activeChats,
