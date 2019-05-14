e7f85ef9d6a21c1b4300e1101a1783dbf51ef2fd
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-05-14 14:49:23 +0100

    Refactor Footer component

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,74 +1,35 @@
 import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import theme from '../../theme/styledTheme';
+import Logo from '../layout/navbar/logo2.svg';
 
-const styles = theme => ({
-  root: {
-    flexGrow: 1,
-    marginTop: 10,
-    textColor: 'ffffff',
-    backgroundColor: '#2f465e',
-    borderTop: 'solid 6px #0CD4AF',
-    paddingTop: '20px',
-    overflowX: 'hiden',
-    color: '#fff',
-    fontSize: '18px',
-    // bottom:'0',
-    // position:'absolute'
-    // borderBottom: "solid 6px #0CD4AF",
-  },
-  footerSections: {
-    margin: '3px',
-    padding: '10px',
-    color: 'rgba(255,255,255)',
-  },
-  subFooter: {
-    paddingBottom: '8px',
-    margin: '20px',
-    color: '#fff',
-    fontSize: '18px',
-  },
-});
-
-class Footer extends React.Component {
-  // date incorperated
-
-  render() {
-    const { classes } = this.props;
-    const currentYear = new Date().getFullYear();
-    return (
-      <div className="container">
-        <div className={classes.root}>
-          <Grid container spacing={24}>
-            <Grid item xs={12} sm={4}>
-              <Typography className={classes.footerSections}>
-                psum dolor sit amet salami ham hock ham, hamburger corned beef
-                short ribs kielbasa biltong
-              </Typography>
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Typography className={classes.footerSections}>
-                psum dolor sit amet salami ham hock ham, hamburger corned beef
-                short ribs kielbasa biltong
-              </Typography>
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Typography className={classes.footerSections}>
-                psum dolor sit amet salami ham hock ham, hamburger corned beef
-                short ribs kielbasa biltong
-              </Typography>
-            </Grid>
-
-            <Grid className={classes.subFooter} item xs={12}>
-              <Typography className={classes.footerSections}>
-                <span> Front Desk Web Chat ©{currentYear}</span>
-              </Typography>
-            </Grid>
-          </Grid>
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <FooterWrapper>
+      <div className="footer-company">
+        <img src={Logo} alt="company-logo"/>
+        <span>© {currentYear} FrontDesk</span>
+        <div className="social-icons">
+          <i className="fab fa-github" />
+          <i className="fab fa-linkedin-in" />
+          <i className="far fa-envelope" />
         </div>
       </div>
-    );
+      <nav className="footer-nav">
+        <Link>About</Link>
+        <Link>Help</Link>
+        <Link>Terms</Link>
+        <Link>Privacy</Link>
+      </nav>
+
+    </FooterWrapper>
+  )
+};
+
+export default Footer;
+
   }
 }
 
