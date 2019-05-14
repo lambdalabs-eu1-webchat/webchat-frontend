05441ed1dbbc4801350f821f4d0422159e49b9b5
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-05-14 14:50:15 +0100

    Add styling for Footer component

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -30,7 +30,86 @@ const Footer = () => {
 
 export default Footer;
 
+const FooterWrapper = styled.div`
+  background: ${theme.color.secondaryPurple};
+  border-top: 4px solid ${theme.color.accentGreen};
+  display: flex;
+  color: ${theme.color.lightPurple};
+  padding: 2% 3%;
+  justify-content: space-between;
+  align-items: center;
+  @media (max-width: 1000px) {
+    flex-direction: column-reverse;
+    padding-top: 0;
   }
-}
+  
+  nav {
+    background: none;
+    padding: 0;
+    width: 30%;
+    display: flex;
+    justify-content: space-between;
+    @media(max-width: 1000px) {
+      width: 85%;
+      justify-content: space-between;
+      border-bottom: 1px solid ${theme.color.footerText};
+      padding: 20px 0;
+    }
+    
+    a {
+      font-size: ${theme.fontSize.xxs};
+      color: ${theme.color.lightPurple};
+      font-weight: bold;
+      margin: 0;
+      padding: 0;
+      &:hover {
+        color: ${theme.color.accentGreen};
+        transition: all 0.3s ease-in;
+        cursor: pointer;
+      }
+    }
+  }
+  
+  .footer-company {
+    display: flex;
+    flex-direction: column;
+    
+    img {
+      width: 60%;
+      @media(max-width: 1000px) {
+        padding-top: 20px;
+        margin: 0 auto;
+      }
+    }
 
-export default withStyles(styles)(Footer);
+     span {
+      color: ${theme.color.footerText};
+      font-size: ${theme.fontSize.xxs};
+      font-weight: bold;
+      padding: 20px 0;
+      @media(max-width: 1000px) {
+        text-align: center;
+      }
+    }
+    
+    .social-icons {
+      @media(max-width: 1000px) {
+        text-align: center;
+        padding-bottom: 20px;
+      }
+      
+      .fab, .far {
+        font-size: ${theme.fontSize.xs};
+        padding: 4%;
+        background: ${theme.color.accentPurple};
+        border-radius: 50%;
+        margin-right: 7px;
+        &:hover {
+          color: ${theme.color.accentGreen};
+          transition: all 0.3s ease-in;
+          cursor: pointer;
+        }
+      }
+    }
+  }
+`;
