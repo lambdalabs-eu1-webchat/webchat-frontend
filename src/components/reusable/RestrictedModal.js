6f0b061bd606bea273db3dc3a4c4cca62421334d
Pav0l <pavol.karas@gmail.com>
2019-05-15 12:59:17 +0200

    Edit styling of the modal content

--- src/components/reusable/RestrictedModal.js
+++ src/components/reusable/RestrictedModal.js
@@ -21,18 +21,43 @@ class Restricted extends React.Component {
           content: modalTheme.restrictedContent,
         }}
       >
-        <RestrictedMsg>{this.props.alert}</RestrictedMsg>
-        <button onClick={this.props.closeRestrictedModal}>Close</button>
+        <ModalWrapper>
+          <RestrictedIcon
+            src="https://image.flaticon.com/icons/svg/1636/1636066.svg"
+            alt="alert"
+          />
+          <RestrictedMsg>{this.props.alert}</RestrictedMsg>
+          <RestrictedBtn onClick={this.props.closeRestrictedModal}>
+            Close
+          </RestrictedBtn>
+        </ModalWrapper>
       </Modal>
     );
   }
 }
 
+const ModalWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const RestrictedIcon = styled.img`
+  max-width: 50px;
+  margin: 10px auto;
+`;
+
 const RestrictedMsg = styled.div`
   line-height: ${theme.spacing.lineHeight};
-  font-size: ${theme.fontSize.m};
-  text-align: center;
+  font-size: ${theme.fontSize.sm};
+  text-align: left;
   color: ${theme.color.textColor};
+  margin-bottom: 1rem;
+`;
+
+const RestrictedBtn = styled.button`
+  align-self: end;
+  width: 100px;
+  color: ${theme.color.white};
 `;
 
 export default Restricted;
