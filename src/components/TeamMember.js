bf36997040f31519f2303543021407fe22b5b5a3
Pav0l <pavol.karas@gmail.com>
2019-05-15 13:55:53 +0200

    Restricted modal to delete user

--- src/components/TeamMember.js
+++ src/components/TeamMember.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+
+import Restricted from './reusable/RestrictedModal';
 
 const HotelStaffWrapper = styled.div`
   display: flex;
@@ -12,59 +14,93 @@ const HotelStaffWrapper = styled.div`
     padding: 20px;
   }
   p {
-      width: 50px;
+    width: 50px;
   }
 `;
 
-const handleAdminPromotion = (changeUserType, id, currentUser, user_type) => {
-  if (currentUser.user_type !== 'super admin') {
-    return () => {};
-  }
-  return (event) => {
-    if (user_type === 'super admin') {
-      event.preventDefault();
-      return;
-    }
-    if (!event.target.checked) {
-      changeUserType(id, 'receptionist');
-    } else {
-      changeUserType(id, 'admin');
-    }
-  };
-};
-const handleDeleteClick = (deleteUser, id, currentUser, user_type) => {
-  if (currentUser.user_type !== 'admin' && currentUser.user_type !== 'super admin') {
-    return () => {};
-  }
-  return (event) => {
-    if (user_type === 'super admin') {
-      return;
-    }
-    if (user_type === 'admin' && currentUser.user_type === 'admin') {
-      alert('You are not authorized to delete this user!');
-      return;
-    }
-    if (window.confirm('Are you sure you want to delete this user?')) {
-      event.preventDefault();
-      deleteUser(id);
-    }
-  }
-};
+const TeamMember = ({
+  name,
+  email,
+  user_type,
+  currentUser,
+  changeUserType,
+  userId,
+  deleteUser,
+}) => {
+  // use Hooks here, as it's already a func component
+  const [isModalOpen, setModalOpen] = useState(false);
 
-const TeamMember = ({ name, email, user_type, currentUser, changeUserType, userId, deleteUser }) => {
   const isAdmin = user_type === 'admin' || user_type === 'super admin';
+
+  const openModal = () => setModalOpen(true);
+  const closeModal = () => setModalOpen(false);
+
+  const handleAdminPromotion = (changeUserType, id, currentUser, user_type) => {
+    if (currentUser.user_type !== 'super admin') {
+      return () => {};
+    }
+    return event => {
+      if (user_type === 'super admin') {
+        event.preventDefault();
+        return;
+      }
+      if (!event.target.checked) {
+        changeUserType(id, 'receptionist');
+      } else {
+        changeUserType(id, 'admin');
+      }
+    };
+  };
+  const handleDeleteClick = (deleteUser, id, currentUser, user_type) => {
+    if (
+      currentUser.user_type !== 'admin' &&
+      currentUser.user_type !== 'super admin'
+    ) {
+      return () => {};
+    }
+    return event => {
+      if (user_type === 'super admin') {
+        return;
+      }
+      if (user_type === 'admin' && currentUser.user_type === 'admin') {
+        openModal();
+        return;
+      }
+      if (window.confirm('Are you sure you want to delete this user?')) {
+        event.preventDefault();
+        deleteUser(id);
+      }
+    };
+  };
+
   return (
     <HotelStaffWrapper>
-      <p>{ name }</p>
-      <p>{ email }</p>
+      <p>{name}</p>
+      <p>{email}</p>
       <input
         type="checkbox"
         checked={isAdmin}
         disabled={user_type === 'super admin'}
-        onChange={handleAdminPromotion(changeUserType, userId, currentUser, user_type)}
+        onChange={handleAdminPromotion(
+          changeUserType,
+          userId,
+          currentUser,
+          user_type
+        )}
       />
-      <i className="fas fa-trash-alt" onClick={handleDeleteClick(deleteUser, userId, currentUser, user_type)}/>
-      </HotelStaffWrapper>
+      <i
+        className="fas fa-trash-alt"
+        onClick={handleDeleteClick(deleteUser, userId, currentUser, user_type)}
+      />
+
+      {isModalOpen && (
+        <Restricted
+          alert="You are not authorized to delete this user!"
+          isRestrictedModalOpen={isModalOpen}
+          closeRestrictedModal={closeModal}
+        />
+      )}
+    </HotelStaffWrapper>
   );
 };
 
