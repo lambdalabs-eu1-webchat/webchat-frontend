255d8ecef7e2c26e9d140ddc6ee017775d1e1c38
Pav0l <pavol.karas@gmail.com>
2019-05-15 22:19:47 +0200

    Create new translate modal

---
+++ src/components/TranslateModal.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import Modal from 'react-modal';
+import styled from 'styled-components';
+
+import modalTheme from '../theme/modalTheme';
+import theme from '../theme/styledTheme';
+
+class TranslateModal extends React.Component {
+  render() {
+    console.log(this.props.translations);
+    return (
+      <Modal
+        // The only required prop
+        // Boolean describing if the modal should be shown or not
+        isOpen={this.props.isTranslateModalOpen}
+        ariaHideApp={false}
+        // Function that will be run when the modal is requested to be closed
+        // (either by clicking on overlay or pressing ESC)
+        onRequestClose={this.props.closeTranslateModal}
+        style={{
+          overlay: modalTheme.translateOverlay,
+          content: modalTheme.translateContent,
+        }}
+      >
+        <ModalWrapper>
+          {this.props.translations.map((message, idx) => (
+            <Msg key={idx}> - {message.translatedText}</Msg>
+          ))}
+
+          <Btn onClick={this.props.closeTranslateModal}>Close</Btn>
+        </ModalWrapper>
+      </Modal>
+    );
+  }
+}
+
+const ModalWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Msg = styled.div`
+  line-height: ${theme.spacing.lineHeight};
+  font-size: ${theme.fontSize.sm};
+  text-align: left;
+  color: ${theme.color.textColor};
+  margin-bottom: 1rem;
+`;
+
+const Btn = styled.button`
+  align-self: end;
+  width: 100px;
+  color: ${theme.color.white};
+`;
+
+export default TranslateModal;
