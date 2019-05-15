52094067bfde832e1b2d1bfe86811ffa6b1889e1
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-05-15 19:09:53 +0100

    Add styling for Login component

--- src/views/Login.js
+++ src/views/Login.js
@@ -68,3 +68,84 @@ export default connect(
   mapStateToProps,
   { loginRequest },
 )(Login);
+
+const LoginOuterWrapper = styled.div`
+  background: ${theme.color.offWhite};
+  height: 100%;
+  @media (max-width: 600px) {
+    background: ${theme.color.white};
+  }
+`;
+
+const LoginWrapper = styled.div`
+  padding: 8rem 0;
+  @media (max-width: 600px) {
+    width: 100%;
+    margin: 0;
+    padding: 0;
+  }
+  
+  form {
+    margin: 0 auto;
+    background: ${theme.color.white};
+    padding: 30px;
+    box-shadow: ${theme.shadow.cardShadow};
+    width: 450px;
+    display: flex;
+    flex-direction: column;
+    border-radius: 3px;
+    @media (max-width: 600px) {
+      width: 100%;
+      height: 100%;
+      margin: auto 0;
+      padding-bottom: 6.5rem; 
+      align-self: center;
+      
+      box-shadow: none;
+    }
+    h2 {
+      font-size: ${theme.fontSize.xl};
+      padding: 20px 0;
+      color: ${theme.color.textColor};
+    }
+    label {
+      font-size: ${theme.fontSize.xxs};
+      color: ${theme.color.accentPurple};
+      font-weight: bold;
+    }
+  
+    input {
+      border: none;
+      border-bottom: 1px solid ${theme.color.footerText};
+      margin-bottom: 20px;
+      height: ${theme.input.height};
+      font-size: ${theme.fontSize.xs};
+      padding: 20px 0;
+      border-radius: 0;
+      &:focus {
+        outline: none;
+      }
+    }
+  
+    button {
+      width: 100%;
+      height: ${theme.button.height};
+      font-size: ${theme.fontSize.s};
+      border-radius: ${theme.border.radius};
+      background:${theme.color.accentGreen};
+      border: none;
+      text-transform: ${theme.textTransform.uppercase};
+      color: ${theme.color.white};
+      font-weight: ${theme.fontWeight.bold};
+      margin: 15px 0;
+      box-shadow: ${theme.shadow.buttonShadow};
+      &:hover {
+      box-shadow: ${theme.shadow.buttonHover};
+      cursor: pointer;
+      }
+      &:focus {
+      outline: none;
+      }
+    }
+  }
+`;
