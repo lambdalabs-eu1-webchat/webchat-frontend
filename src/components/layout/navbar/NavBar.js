b7995dc804b1e0501d050f02d2e273544aec3423
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-05-15 08:24:35 +0100

    Add styling to NavBar component

--- src/components/layout/navbar/NavBar.js
+++ src/components/layout/navbar/NavBar.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import logo from './logo2.svg';
 import { NavLink } from 'react-router-dom';
 import LoggedIn from './SignedInLink';
 import LoggedOut from './SignedOutLink';
 import { connect } from 'react-redux';
+import theme from '../../../theme/styledTheme';
 
 const NavBar = props => {
   return (
@@ -37,15 +39,6 @@ const NavBar = props => {
   );
 };
 
-const StyledNav = styled.nav`
-  .tickets {
-    color: #0b9fec;
-  }
-  .active {
-    color: white;
-  }
-`;
-
 function mapStateToProps(state) {
   return {
     numberActiveTickets: state.chats.activeChats.length,
@@ -54,3 +47,30 @@ function mapStateToProps(state) {
 }
 
 export default connect(mapStateToProps)(NavBar);
+
+const StyledNav = styled.nav`
+  background: ${theme.color.secondaryPurple};
+  padding: 1rem 2.5rem;
+  text-transform: uppercase;
+  a {
+    text-decoration: none;
+  }
+  .container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    
+    img {
+      border-radius: 0;
+      width: 60%;
+    }
+  }
+
+  .tickets {
+    color: ${theme.color.accentGreen};
+  }
+  .active {
+    color: white;
+  }
+`;
