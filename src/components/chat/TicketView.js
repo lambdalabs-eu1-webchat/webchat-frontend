293562839e7bde91e5be37ff31ad5d65cebb1ed5
Talent Antonio <antonio.talenty@gmail.com>
2019-05-15 15:42:53 +0100

    change the ticket

--- src/components/chat/TicketView.js
+++ src/components/chat/TicketView.js
@@ -62,7 +62,7 @@ TicketView.propTypes = {
 
 const divStyle = {
   border:'1px solid #0CD4AF',
-  backgroundColor:'#c7ccec',
+  backgroundColor:'#5B66AC',
   margin: '10px',
   padding: '10px',
   width: '100%',
