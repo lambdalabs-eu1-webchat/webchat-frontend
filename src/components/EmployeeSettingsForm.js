ed31ba7247991e8ef88fcd431bbbe7417757915f
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-05-16 09:03:59 +0100

    Add styling to EmployeeSettingsForm component

--- src/components/EmployeeSettingsForm.js
+++ src/components/EmployeeSettingsForm.js
@@ -4,10 +4,6 @@ import TextField from '@material-ui/core/TextField';
 import styled from 'styled-components';
 import theme from './../theme/styledTheme';
 
-const EmployeeSettingsFormWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
 
 const EmployeeSettingsForm = ({
   employeeChanges,
@@ -66,3 +62,70 @@ const EmployeeSettingsForm = ({
 };
 
 export default EmployeeSettingsForm;
+
+const EmployeeSettingsFormWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  width: 50rem;
+  height: 60vh;
+  @media (max-width: 1000px) {
+    width: 100%;
+    height: 100%;
+  }
+  
+  h1 {
+    font-size: ${theme.fontSize.xl};
+    padding: 20px 0;
+    color: ${theme.color.textColor};
+  }
+  
+  input {
+    border: none;
+    border-bottom: 1px solid ${theme.color.footerText};
+    margin-bottom: 20px;
+    width: 100%;
+    height: ${theme.input.height};
+    font-size: ${theme.fontSize.xs};
+    padding: 20px 0;
+    border-radius: 0;
+    &:focus {
+      outline: none;
+    }
+  }
+  
+  .form-buttons {
+    display: flex;
+    @media(max-width: 600px) {
+      flex-direction: column;
+      width: 100%;
+      margin: 0;
+    }
+    button {
+      width: 15rem;
+      height: ${theme.button.height};
+      font-size: ${theme.fontSize.s};
+      border-radius: ${theme.border.radius};
+      background:${theme.color.accentGreen};
+      border: none;
+      text-transform: ${theme.textTransform.uppercase};
+      color: ${theme.color.white};
+      font-weight: ${theme.fontWeight.bold};
+      margin: 15px 0;
+      box-shadow: ${theme.shadow.buttonShadow};
+      &:hover {
+        box-shadow: ${theme.shadow.buttonHover};
+        cursor: pointer;
+      }
+      &:focus {
+        outline: none;
+      }
+      &:first-child {
+      margin-right: 1.5rem;
+      }
+      @media (max-width: 600px) {
+        width: 100%;
+      }
+    }
+  } 
+`;
