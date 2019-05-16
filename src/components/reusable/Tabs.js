48b5c3478faf52dca5fa074c821ba5e43993595d
Talent Antonio <antonio.talenty@gmail.com>
2019-05-16 12:45:13 +0100

    tab fixed

--- src/components/reusable/Tabs.js
+++ src/components/reusable/Tabs.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import theme from '../../theme/styledTheme'
 
 function Tabs({ options, selected, setSelected }) {
   return (
@@ -20,14 +21,15 @@ function Tabs({ options, selected, setSelected }) {
 const StyledTabs = styled.div`
   .tab {
    margin:2rem;
-   padding:1rem;
+   padding:1.5rem;
    position: relative;
    width: 100%;
-   font-weight: 300;
+   font-weight: 700;
+   color: ${theme.color.accentPurple}
    text-transform: uppercase;
   }
   .selected {
-    background: #0CD4AF;
+    background: ${theme.color.white};
 
   }
 `;
