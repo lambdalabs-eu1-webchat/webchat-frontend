125d10eafc63bbfbafd943a1b36376f91fd798c3
mark-marshall <mark.marshallgp@gmail.com>
2019-05-16 15:04:51 +0100

    Amended user actionCreators and added associated cases to loading reducer

--- src/store/reducers/loading.js
+++ src/store/reducers/loading.js
@@ -9,6 +9,20 @@ import {
   FETCH_SINGLE_HOTEL_FINISHED,
   ROOMS,
   SUBSCRIPTIONS,
+  FETCH_ALL_USERS_STARTED,
+  FETCH_ALL_USERS_FINISHED,
+  FETCH_SINGLE_USER_STARTED,
+  FETCH_SINGLE_USER_FINISHED,
+  FETCH_HOTEL_STAFF_STARTED,
+  FETCH_HOTEL_STAFF_FINISHED,
+  CREATE_USER_STARTED,
+  CREATE_USER_FINISHED,
+  UPDATE_USER_STARTED,
+  UPDATE_USER_FINISHED,
+  CHANGE_USER_TYPE_STARTED,
+  CHANGE_USER_TYPE_FINISHED,
+  DELETE_USER_STARTED,
+  DELETE_USER_FINISHED,
 } from '../actions/actionTypes';
 
 const loading = (state = false, action) => {
@@ -56,6 +70,34 @@ const loading = (state = false, action) => {
     case SUBSCRIPTIONS.UPDATE_CUSTOMER_METHOD_STARTED:
       return true;
     case SUBSCRIPTIONS.UPDATE_CUSTOMER_METHOD_FINISHED:
+      return false;
+    case FETCH_ALL_USERS_STARTED:
+      return true;
+    case FETCH_ALL_USERS_FINISHED:
+      return false;
+    case FETCH_SINGLE_USER_STARTED:
+      return true;
+    case FETCH_SINGLE_USER_FINISHED:
+      return false;
+    case FETCH_HOTEL_STAFF_STARTED:
+      return true;
+    case FETCH_HOTEL_STAFF_FINISHED:
+      return false;
+    case CREATE_USER_STARTED:
+      return true;
+    case CREATE_USER_FINISHED:
+      return false;
+    case UPDATE_USER_STARTED:
+      return true;
+    case UPDATE_USER_FINISHED:
+      return false;
+    case CHANGE_USER_TYPE_STARTED:
+      return true;
+    case CHANGE_USER_TYPE_FINISHED:
+      return false;
+    case DELETE_USER_STARTED:
+      return true;
+    case DELETE_USER_FINISHED:
       return false;
     default:
       return state;
