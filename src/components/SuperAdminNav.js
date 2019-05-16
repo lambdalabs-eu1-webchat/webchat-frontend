6643a3f5f9b393d4f0c0d99f254d0966dd8919f7
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-05-16 14:40:43 +0100

    Style SuperAdminNav component

--- src/components/SuperAdminNav.js
+++ src/components/SuperAdminNav.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
+import theme from './../theme/styledTheme';
 
 import { APP_PATHS } from '../utils/paths';
 
@@ -9,7 +11,7 @@ const SuperAdminNav = ({ numberRooms }) => {
     return null;
   }
   return (
-    <div className="super-admin-nav">
+    <SuperAdminNavWrapper>
       <NavLink to={`${APP_PATHS.COMPANY_DASH}${APP_PATHS.TEAM_MEMBERS}`}>
         Team Members
       </NavLink>
@@ -19,7 +21,7 @@ const SuperAdminNav = ({ numberRooms }) => {
       <NavLink to={`${APP_PATHS.COMPANY_DASH}${APP_PATHS.COMPANY_SETTINGS}`}>
         Company Settings
       </NavLink>
-    </div>
+    </SuperAdminNavWrapper>
   );
 };
 
@@ -30,3 +32,39 @@ function mapStateToProps(state) {
 }
 
 export default connect(mapStateToProps)(SuperAdminNav);
+
+const SuperAdminNavWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 5rem;
+  @media (max-width: 600px) {
+    flex-direction: column;
+    width: 100%;
+  }
+  a {
+    text-decoration: none;
+    color: ${theme.color.accentPurple};
+    text-transform: uppercase;
+    padding: 1rem 0;
+    width: 15rem;
+    font-weight: bold;
+    text-align: center;
+    border-radius: 5px 5px 0 0;
+    background: ${theme.color.lightPurple};
+    margin-right: 0.5rem;
+    font-size: ${theme.fontSize.xxs};
+    &.active {
+    color: ${theme.color.accentPurple};
+      border-bottom: 2px solid ${theme.color.accentGreen};
+      font-weight: bold;
+    }
+    @media(max-width: 600px) {
+      width: 100%;
+      background: ${theme.color.lightPurple};
+      margin-bottom: 1rem;
+      &.active {
+        border-bottom: 2px solid ${theme.color.accentGreen};
+      }
+    }
+  }
+`;
