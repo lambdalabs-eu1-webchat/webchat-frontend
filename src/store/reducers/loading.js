4fd41fc6846b05a961599a49590476a464060bd4
mark-marshall <mark.marshallgp@gmail.com>
2019-05-16 14:28:42 +0100

    Amended hotel actioncreators and added associated cases to loading reducer

--- src/store/reducers/loading.js
+++ src/store/reducers/loading.js
@@ -3,6 +3,10 @@ import {
   LOGIN_REQUEST_FINISHED,
   REGISTER_USER_STARTED,
   REGISTER_USER_FINISHED,
+  UPDATE_HOTEL_STARTED,
+  UPDATE_HOTEL_FINISHED,
+  FETCH_SINGLE_HOTEL_STARTED,
+  FETCH_SINGLE_HOTEL_FINISHED,
 } from '../actions/actionTypes';
 
 const loading = (state = false, action) => {
@@ -14,6 +18,14 @@ const loading = (state = false, action) => {
     case REGISTER_USER_STARTED:
       return true;
     case REGISTER_USER_FINISHED:
+      return false;
+    case UPDATE_HOTEL_STARTED:
+      return true;
+    case UPDATE_HOTEL_FINISHED:
+      return false;
+    case FETCH_SINGLE_HOTEL_STARTED:
+      return true;
+    case FETCH_SINGLE_HOTEL_FINISHED:
       return false;
     default:
       return state;
