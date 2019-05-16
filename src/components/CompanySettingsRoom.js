7dee623c1d1b5ecd28f56af192f18719db15924c
conconrob <connortprobertson@gmail.com>
2019-05-16 11:17:53 +0100

    room update uses confirm modal

--- src/components/CompanySettingsRoom.js
+++ src/components/CompanySettingsRoom.js
@@ -3,38 +3,23 @@ import Confirm from './reusable/ConfirmModal';
 
 const CompanySettingsRoom = ({
   room,
-  rooms,
   index,
   name,
   currentUser,
-  handleRoomInputChange,
   deleteRoomForHotel,
   updateRoomForHotel,
 }) => {
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [isUpdateModalOpen, setIsUpdateModalOpen] = useState(false);
+  const [inputValue, setInputValue] = useState(name);
   const handleDeleteClick = () => {
     setIsDeleteModalOpen(true);
   };
-  const handleUpdateClick = (updateRoomForHotel, id, hotelId) => {
-    return event => {
-      event.preventDefault();
-      const roomName = getRoomNameFromInput(event.target.parentNode);
-      if (window.confirm('Are you sure you want to edit this room?')) {
-        if (roomName) {
-          updateRoomForHotel(hotelId, id, roomName);
-        }
-      }
-    };
+  const handleUpdateClick = () => {
+    setIsUpdateModalOpen(true);
+    // updateRoomForHotel(hotelId, id, roomName);
   };
-  const getRoomNameFromInput = parentNode => {
-    let roomName = '';
-    parentNode.childNodes.forEach(childNode => {
-      if (childNode.name === 'roomName') {
-        roomName = childNode.value;
-      }
-    });
-    return roomName;
-  };
+
   return !room || !room[index] ? (
     <div>Loading</div>
   ) : (
@@ -42,26 +27,28 @@ const CompanySettingsRoom = ({
       <input
         placeholder="room name"
         name="roomName"
-        value={rooms[index].name}
-        onChange={event => handleRoomInputChange(event, index)}
+        value={inputValue}
+        onChange={event => setInputValue(event.target.value)}
       />
-      <i
-        className="far fa-edit"
-        onClick={handleUpdateClick(
-          updateRoomForHotel,
-          currentUser.hotel_id,
-          room,
-          name,
-        )}
-      />
+      <i className="far fa-edit" onClick={handleUpdateClick} />
       <i className="fas fa-trash-alt" onClick={handleDeleteClick} />
       {
-        <Confirm
-          isOpen={isDeleteModalOpen}
-          question={`Are you sure you want to delete room ${name}`}
-          closeModal={() => setIsDeleteModalOpen(false)}
-          yesCallBack={() => deleteRoomForHotel(room, currentUser.hotel_id)}
-        />
+        <React.Fragment>
+          <Confirm
+            isOpen={isDeleteModalOpen}
+            question={`Are you sure you want to delete room ${name}?`}
+            closeModal={() => setIsDeleteModalOpen(false)}
+            yesCallBack={() => deleteRoomForHotel(room, currentUser.hotel_id)}
+          />
+          <Confirm
+            isOpen={isUpdateModalOpen}
+            question={`Are you sure you want to update room ${name} to ${inputValue}?`}
+            closeModal={() => setIsUpdateModalOpen(false)}
+            yesCallBack={() =>
+              updateRoomForHotel(room, currentUser.hotel_id, inputValue)
+            }
+          />
+        </React.Fragment>
       }
     </div>
   );
