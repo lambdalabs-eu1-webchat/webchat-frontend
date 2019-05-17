908b95b2c4bde985389c4d5b2cb13f062116b348
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-05-17 13:06:43 +0100

    Add extra media queries to CardDetails and change styling for button

--- src/components/CardDetails.js
+++ src/components/CardDetails.js
@@ -3,53 +3,6 @@ import PT from 'prop-types';
 import styled from 'styled-components';
 import theme from '../theme/styledTheme';
 
-const CardDetailsWrapper = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  flex-wrap: wrap;
-  width: 100%;
-  max-width: 660px;
-  margin: 0 2rem;
-
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: flex-start;
-
-  h3 {
-    color: ${theme.color.accentText};
-    text-align: left;
-    font-size: ${theme.fontSize.s};
-    font-weight: bold;
-  }
-
-  span {
-    font-size: ${theme.fontSize.xs};
-    margin: 0.5rem 0;
-  }
-
-  button {
-    width: 15rem;
-    padding: 1rem;
-    margin: 2rem 0;
-    font-size: ${theme.fontSize.xxs};
-    border-radius: ${theme.border.radius};
-    background: ${theme.color.accentGreen};
-    border: none;
-    text-transform: ${theme.textTransform.uppercase};
-    color: ${theme.color.white};
-    font-weight: ${theme.fontWeight.bold};
-    box-shadow: ${theme.shadow.buttonShadow};
-    &:hover {
-      box-shadow: ${theme.shadow.buttonHover};
-      cursor: pointer;
-    }
-    &:focus {
-      outline: none;
-    }
-  }
-`;
-
 const CardDetails = ({ card, email, handleModalSwitch }) => {
   return (
     <div>
@@ -81,3 +34,56 @@ CardDetails.propTypes = {
 };
 
 export default CardDetails;
+
+const CardDetailsWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  width: 100%;
+  max-width: 300px;
+  margin: 0;
+  padding: 5rem 0;
+  flex-direction: column;
+  align-items: flex-start;
+  @media (max-width: 600px) {
+  max-width: 100%;
+  }
+
+  h3 {
+    color: ${theme.color.accentText};
+    text-align: left;
+    font-size: ${theme.fontSize.s};
+    font-weight: bold;
+  }
+
+  span {
+    font-size: ${theme.fontSize.xs};
+    margin: 0.5rem 0;
+  }
+
+  button {
+    width: 15rem;
+    margin: 2rem 0;
+    font-size: ${theme.fontSize.xxs};
+    border-radius: ${theme.border.radius};
+    background: ${theme.color.accentGreen};
+    border: none;
+    text-transform: ${theme.textTransform.uppercase};
+    color: ${theme.color.white};
+    font-weight: ${theme.fontWeight.bold};
+    box-shadow: ${theme.shadow.buttonShadow};
+    height: ${theme.button.smallButton};
+    &:hover {
+      box-shadow: ${theme.shadow.buttonHover};
+      cursor: pointer;
+    }
+    &:focus {
+      outline: none;
+    }
+    @media (max-width: 600px) {
+    width: 100%;
+    height: ${theme.button.height};
+    font-size: ${theme.fontSize.xs};
+    }
+  }
+`;
