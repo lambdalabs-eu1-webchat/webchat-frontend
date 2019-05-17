b8a9b1f3a4e5e84705af9fdc223f9ba2230830c5
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-05-17 08:53:47 +0100

    Style CheckInForm

--- src/components/GuestCheckInForm.js
+++ src/components/GuestCheckInForm.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
+import theme from './../theme/styledTheme';
 import propTypes from 'prop-types';
 import jwt from 'jsonwebtoken';
 import QRCode from 'qrcode.react';
 import axios from 'axios';
 
-import { DOMAIN, USERS, HOTEL, GUEST_CLIENT_DOMAIN } from '../utils/paths';
-import Select from '@material-ui/core/Select';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
+import { DOMAIN, USERS, GUEST_CLIENT_DOMAIN } from '../utils/paths';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
 class CheckInForm extends React.Component {
@@ -74,9 +72,7 @@ class CheckInForm extends React.Component {
   render() {
     return (
       <CheckInFormWrapper>
-        <Select
-          native={true}
-          displayEmpty={true}
+        <select
           className={
             this.state.errorRoom ? 'error hide-on-print' : 'hide-on-print'
           }
@@ -91,27 +87,26 @@ class CheckInForm extends React.Component {
               Room: {room.name}
             </option>
           ))}
-        </Select>
-        <TextField
+        </select>
+        <input
           placeholder="Name"
           className={
             this.state.errorName ? 'error hide-on-print' : 'hide-on-print'
           }
           onChange={event => this.setNameInput(event.target.value)}
           value={this.state.nameInput}
-          margin="normal"
         />
         {this.state.isCheckingIn ? (
           <CircularProgress />
         ) : (
-          <Button
+          <button
             className="hide-on-print"
             variant="contained"
             color="primary"
             onClick={this.checkInGuest}
           >
             Check In
-          </Button>
+          </button>
         )}
         <p className="show-on-print">https://webchatlabs-guest.netlify.com</p>
         <div className="passcode">
@@ -131,23 +126,93 @@ CheckInForm.propTypes = {
 const CheckInFormWrapper = styled.div`
   display: flex;
   flex-direction: column;
+  select {
+    padding: 3rem;
+    background: ${theme.color.lightPurple};
+    font-size: ${theme.fontSize.xxs};
+    color: ${theme.color.accentPurple};
+    font-family: ${theme.font.fontFamily};
+    font-weight: bold;
+    height: 3rem;
+    border: none;
+    &:focus {
+    outline: none;
+    }
+  }
+  input {
+    border: none;
+    border-bottom: 1px solid ${theme.color.footerText};
+    margin: 2rem 0;
+    height: ${theme.input.height};
+    font-size: ${theme.fontSize.xs};
+    padding: 20px 0;
+    border-radius: 0;
+    &:focus {
+      outline: none;
+    }
+  }
+  //.hide-on-print {
+  //  padding: 30px;
+  //}
   button {
-    margin-top: 10%;
+    width: 100%;
+    height: ${theme.button.smallButton};
+    font-size: ${theme.fontSize.xxs};
+    border-radius: ${theme.border.radius};
+    background:${theme.color.accentGreen};
+    border: none;
+    text-transform: ${theme.textTransform.uppercase};
+    color: ${theme.color.white};
+    font-weight: ${theme.fontWeight.bold};
+    margin: 15px 0;
+    box-shadow: ${theme.shadow.buttonShadow};
+    &:hover {
+      box-shadow: ${theme.shadow.buttonHover};
+      cursor: pointer;
+    }
+    &:focus {
+      outline: none;
+    }
+    &:first-child {
+      margin-right: 1.5rem;
+    }
+    @media (max-width: 1200px) {
+      width: 100%;
+      height: ${theme.button.smallButton};
+      margin: 0 0 1.5rem 0;
+      &:first-child {
+        margin-right: 0;
+      }
+    }
+    @media (max-width: 800px) {
+      height: ${theme.button.height};
+      font-size: ${theme.fontSize.xs};
+    }
   }
+  
   .error {
-    background: red;
+    box-shadow: 0 0 3px red;
   }
+  
   button {
     margin-bottom: 5%;
   }
+  
   .passcode {
-    width: 90%;
-    background-color: #aed581;
-    height: 50px;
-    border-radius: 1%;
-    padding: 5%;
+    width: 100%;
+    background: ${theme.color.lightPurple};
+    height: 8rem;
+    border-radius: 5px;
+    padding: 3rem;
+    margin-bottom: 3rem;
+    h4 {
+      font-size: ${theme.fontSize.xxs};
+      align-self: center;
+    }
+    
     p {
       text-align: center;
+      padding-bottom: 1.5rem;
     }
   }
   canvas {
