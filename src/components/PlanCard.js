d87c7efd414f044adb1a1f9399afa2ec0a6e1bd2
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-05-17 13:05:38 +0100

    Add extra media queries and change button heights to match the company dash

--- src/components/PlanCard.js
+++ src/components/PlanCard.js
@@ -3,79 +3,6 @@ import PT from 'prop-types';
 import styled from 'styled-components';
 import theme from '../theme/styledTheme';
 
-const HighlighterWrapper = styled.div`
-  background-color: ${props => props.highlight};
-  border-radius: 5px;
-  text-align: center;
-  width: 30%;
-  max-width: 320px;
-  padding: 2rem;
-  margin: 2rem;
-  box-shadow: 0 16px 48px rgba(32, 41, 50, 0.21);
-  @media (max-width: 1000px) {
-    padding: 4rem;
-    margin: 3rem auto;
-    width: 100%;
-  }
-
-  h2 {
-    font-weight: bold;
-    text-align: center;
-    font-size: ${theme.fontSize.m};
-    color: ${theme.color.accentText};
-  }
-
-  ul {
-    padding: 0 5%;
-
-    li {
-      font-size: ${theme.fontSize.xs};
-      padding: 5% 0;
-      text-align: left;
-    }
-  }
-
-  .fa-check {
-    background: #e2fdf8;
-    border: 1px solid white;
-    border-radius: 50%;
-    padding: 2.5%;
-    color: ${theme.color.accentGreen};
-    font-size: 1rem;
-    margin-right: 2.5%;
-  }
-
-  button {
-    width: 15rem;
-    min-width: 150px;
-    padding: 2rem;
-    font-size: ${theme.fontSize.xxs};
-    border-radius: ${theme.border.radius};
-    background: ${theme.color.accentGreen};
-    border: none;
-    text-transform: ${theme.textTransform.uppercase};
-    color: ${theme.color.white};
-    font-weight: ${theme.fontWeight.bold};
-    margin: 15px 0;
-    box-shadow: ${theme.shadow.buttonShadow};
-    &:hover {
-      box-shadow: ${theme.shadow.buttonHover};
-      cursor: pointer;
-    }
-    &:focus {
-      outline: none;
-    }
-  }
-`;
-
-const PriceTag = styled.div`
-  color: ${theme.color.accentText};
-  text-align: center;
-  font-size: ${theme.fontSize.s};
-  font-weight: bold;
-  padding: 5% 0;
-`;
-
 const PlanCard = ({ plan, current, fireSwitchCustomerPlan }) => {
   return (
     <HighlighterWrapper
@@ -114,3 +41,84 @@ PlanCard.propTypes = {
 };
 
 export default PlanCard;
+
+const HighlighterWrapper = styled.div`
+  background-color: ${props => props.highlight};
+  border-radius: 5px;
+  text-align: center;
+  width: 30%;
+  max-width: 320px;
+  padding: 2rem;
+  margin: 2rem;
+  box-shadow: 0 16px 48px rgba(32, 41, 50, 0.21);
+  @media (max-width: 1000px) {
+    padding: 4rem;
+    margin: 3rem auto;
+    width: 100%;
+  }
+  @media (min-width: 1200px) {
+  height: 400px;
+  }
+
+  h2 {
+    font-weight: bold;
+    text-align: center;
+    font-size: ${theme.fontSize.m};
+    color: ${theme.color.accentText};
+  }
+
+  ul {
+    li {
+      font-size: ${theme.fontSize.xs};
+      padding: 5% 0;
+      text-align: left;
+    }
+  }
+
+  .fa-check {
+    background: #e2fdf8;
+    border: 1px solid white;
+    border-radius: 50%;
+    padding: 2.5%;
+    color: ${theme.color.accentGreen};
+    font-size: 1rem;
+    margin-right: 2.5%;
+  }
+
+  button {
+    width: 100%;
+    min-width: 150px;
+    height: ${theme.button.smallButton};
+    font-size: ${theme.fontSize.xxs};
+    border-radius: ${theme.border.radius};
+    background: ${theme.color.accentGreen};
+    border: none;
+    text-align: center;
+    text-transform: ${theme.textTransform.uppercase};
+    color: ${theme.color.white};
+    font-weight: ${theme.fontWeight.bold};
+    margin: 15px 0;
+    box-shadow: ${theme.shadow.buttonShadow};
+    &:hover {
+      box-shadow: ${theme.shadow.buttonHover};
+      cursor: pointer;
+    }
+    &:focus {
+      outline: none;
+    }
+    
+    @media (max-width: 600px) {
+      width: 100%;
+      height: ${theme.button.height};
+      font-size: ${theme.fontSize.xs};
+    }
+  }
+`;
+
+const PriceTag = styled.div`
+  color: ${theme.color.accentText};
+  text-align: center;
+  font-size: ${theme.fontSize.s};
+  font-weight: bold;
+  padding: 5% 0;
+`;
