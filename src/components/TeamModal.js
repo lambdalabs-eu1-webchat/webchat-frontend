53b59c474fbfec1b21d6b5575929fff59bd1993c
Talent Antonio <antonio.talenty@gmail.com>
2019-05-22 00:21:10 +0100

    default styling

--- src/components/TeamModal.js
+++ src/components/TeamModal.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import theme from '../theme/styledTheme';
+import styled from 'styled-components';
 
 export default class TeamModal extends React.Component {
 
@@ -11,6 +12,7 @@ export default class TeamModal extends React.Component {
             return null;
         }
         return (
+            <bigContainer>
             <div style={backdropStyle}>
                 <div style={modalStyle}>           
 
@@ -66,9 +68,168 @@ export default class TeamModal extends React.Component {
               </div>
             </div>
         </div>
+        </bigContainer>
         )
     }
 }
+
+const bigContainer = styled.div`
+  background: ${theme.color.white};
+  margin: 0 5rem;
+  box-shadow: ${theme.shadow.containerShadow};
+
+  h1 {
+    font-size: ${theme.fontSize.xl};
+    padding: 25px;
+    font-weight: bold;
+    width: 60%;
+    color: ${theme.color.textColor};
+    @media (max-width: 1000px) {
+      width: 90%;
+    }
+  }
+  h2 {
+    font-size: ${theme.fontSize.l};
+    color: ${theme.color.textColor};
+  }
+  h3 {
+    font-size: ${theme.fontSize.m};
+    color: ${theme.color.textColor};
+  }
+  p {
+    font-size: ${theme.fontSize.m};
+    color: ${theme.color.textColor};
+    padding: 25px;
+  }
+  
+  a {
+    text-decoration: none;
+    color: ${theme.color.white};
+    font-weight: bold;
+  }
+
+    button {
+      margin: 25px;
+      padding: 10px 20px;
+      width: 20rem;
+      border-radius: 5px;
+      height: ${theme.button.height};
+      font-size: ${theme.fontSize.xs};
+      border: none;
+      text-transform: uppercase;
+      color: ${theme.color.white};
+      background: ${theme.color.accentGreen};
+      font-weight: bold;
+      box-shadow: ${theme.shadow.buttonShadow};
+      &:hover {
+        box-shadow: ${theme.shadow.buttonHover};
+        background: ${theme.color.accentPurple};
+        cursor: pointer;
+        transition: all 0.3s ease;
+      }
+      @media(max-width: 600px) {
+        width: 90%;
+        margin: 5%;
+      }
+    }
+    a {
+    align-self: center;
+    }
+  }
+
+  .pricing-plan {
+    padding: 0 2.5% 5% 2.5%;
+    @media (max-width: 1000px) {
+      width: 95%;
+      padding: 10% 0 15% 0;
+      margin: 0 auto;
+    }
+      
+    .pricing-cards {
+      display: flex;
+      justify-content: space-around;
+      margin: 0 2.5%;
+      @media (max-width: 1000px) {
+        flex-direction: column;
+        width: 90%;
+        margin: 0 5%;
+      }
+    }
+    
+    .pricing-card {
+      background: ${theme.color.lightPurple};
+      border-radius: 3px;
+      text-align: center;
+      width: 30%;
+      padding: 2.5%;
+      margin: 2.5%;
+      box-shadow: 0 16px 48px rgba(32, 41, 50, 0.21);
+      @media (max-width: 1000px) {
+        padding: 10%;
+        margin: 10% auto;
+        width: 100%;
+      }
+      
+      h2 {
+        font-weight: bold;
+        text-align: center;
+        font-size: ${theme.fontSize.m};
+        color: ${theme.color.accentText};
+      }
+      
+      ul {
+        padding: 0 5%;
+        
+        li {
+          font-size: ${theme.fontSize.xs};
+          padding: 5% 0;
+          text-align: left;
+        }
+      }
+      
+      h3 {
+        color: ${theme.color.accentText};
+        text-align: center;
+        font-size: ${theme.fontSize.s};
+        font-weight: bold;
+        padding: 5% 0;
+      }
+      
+      button {
+        margin: 5% 0;
+        height: ${theme.button.height};
+        width: 100%;
+        color: ${theme.color.white};
+        border: none;
+        background: ${theme.color.accentGreen};
+        text-transform: uppercase;
+        font-size: ${theme.fontSize.xs};
+        border-radius: ${theme.border.radius};
+        font-weight: bold;
+        box-shadow: ${theme.shadow.buttonShadow};
+        &:hover {
+          box-shadow: ${theme.shadow.buttonHover};
+          background: ${theme.color.accentPurple};
+          cursor: pointer;
+          transition: all 0.3s ease;
+        }
+        @media(max-width: 500px) {
+          margin: 0 auto;  
+        }
+      }
+      
+      .fa-check {
+        background: #e2fdf8;
+        border: 1px solid white;
+        border-radius: 50%;
+        padding: 2.5%;
+        color: ${theme.color.accentGreen};
+        font-size: 1rem;
+        margin-right: 2.5%;
+      }
+    }
+  }
+`;
 const backdropStyle = {
     position:'fixed',
     top:0,
