5acabcc2b41c564f76b4392e652cf66ff4dec485
Talent Antonio <antonio.talenty@gmail.com>
2019-05-22 21:40:05 +0100

    styling

--- src/views/About.js
+++ src/views/About.js
@@ -16,39 +16,39 @@ class About extends React.Component {
         <PageContainer>
         <section className="about-container">
             <div class="card">
-              <img src={diana} alt=""/>
-               <h2>Talent Antonio</h2>
-               <p>Full Stack Developer</p>
-               <a href="https://github.com/sampler36"><i class="fab fa-github gh"></i></a>
+              <img src={luke} alt=""/>
+               <h2>Lukasz Busk</h2>
+               <p>Project Manager</p>
+               <a href="https://github.com/Velsu"><i class="fab fa-github gh"></i></a>
                <a href="https://twitter.com"><i class="fab fa-twitter tw "></i></a>
                </div>  
                <div class="card">
-              <img src={talent} alt=""/>
-               <h2>Talent Antonio</h2>
+              <img src={diana} alt=""/>
+               <h2>Diana Ignatescu</h2>
                <p>Full Stack Developer</p>
-               <a href="https://github.com/sampler36"><i class="fab fa-github gh"></i></a>
+               <a href="https://github.com/DianaIgnatescu"><i class="fab fa-github gh"></i></a>
                <a href="https://twitter.com"><i class="fab fa-twitter tw "></i></a>
                </div> 
                <div class="card">
               <img src={mark} alt=""/>
-               <h2>Talent Antonio</h2>
+               <h2>Mark Marshall</h2>
                <p>Full Stack Developer</p>
-               <a href="https://github.com/sampler36"><i class="fab fa-github gh"></i></a>
+               <a href="https://github.com/mark-marshall"><i class="fab fa-github gh"></i></a>
                <a href="https://twitter.com"><i class="fab fa-twitter tw "></i></a>
                </div> 
                <div class="card">
               <img src={connor} alt=""/>
-               <h2>Talent Antonio</h2>
+               <h2>Connor Robertson</h2>
                <p>Full Stack Developer</p>
-               <a href="https://github.com/sampler36"><i class="fab fa-github gh"></i></a>
+               <a href="https://github.com/ConConRob"><i class="fab fa-github gh"></i></a>
                <a href="https://twitter.com"><i class="fab fa-twitter tw "></i></a>
                </div> 
                <div class="card">
               <img src={pavol} alt=""/>
-               <h2>Talent Antonio</h2>
+               <h2>Pavol Karas</h2>
                <p>Full Stack Developer</p>
-               <a href="https://github.com/sampler36"><i class="fab fa-github gh"></i></a>
-               <a href="https://twitter.com"><i class="fab fa-twitter tw "></i></a>
+               <a href="https://github.com/Pav0l"><i class="fab fa-github gh"></i></a>
+               <a href="https://twitter.com/KarasPavol"><i class="fab fa-twitter tw "></i></a>
                </div> 
                <div class="card">
               <img src={talent} alt=""/>
@@ -67,31 +67,38 @@ class About extends React.Component {
 export default About;
 
 const PageOuter = styled.div`
-  background: ${theme.color.offWhite};
+background: ${theme.color.white};
 `;
 
 const PageContainer = styled.div`
-  background: ${theme.color.white};
+  background: ${theme.color.secondaryPurple};
   margin: 0 5rem;
   box-shadow: ${theme.shadow.containerShadow};
+  border:none;
 
   section {
-    background-color: #e9e9e9;
     text-align: center;
     padding: 5px 2px;
+    border:none;
 }
   .about-container {
       display:flex;
       justify-content:space-around;
       padding: 100px 0;
       flex-flow:row wrap;
+      
+      .card{
+        padding: 20px;
+        border-bottom: #08addd 2px solid;
+        margin: 16px 10px;
+      }
        
       img{
         width: 180px;
         border-radius: 60%;
       }
       h1 {
-        font-size: ${theme.fontSize.xl};
+        font-size: ${theme.fontSize.m};
         padding: 25px;
         font-weight: bold;
         width: 60%;
@@ -102,11 +109,7 @@ const PageContainer = styled.div`
       }
       h2 {
         font-size: ${theme.fontSize.l};
-        color: ${theme.color.textColor};
-      }
-      h3 {
-        font-size: ${theme.fontSize.m};
-        color: ${theme.color.textColor};
+        color: ${theme.color.white};
       }
       p {
         font-size: ${theme.fontSize.xs};
@@ -116,7 +119,7 @@ const PageContainer = styled.div`
     
       a {
         text-decoration: none;
-        color: ${theme.color.accentPurple};
+        color: ${theme.color.footerText};
         size:500px;
         font-weight: bold;
       }
