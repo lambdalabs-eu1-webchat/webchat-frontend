8d2584235d76b6e38f4062b82757b250911bce89
Talent Antonio <antonio.talenty@gmail.com>
2019-05-23 10:34:46 +0100

    theme change

--- src/theme/styledTheme.js
+++ src/theme/styledTheme.js
@@ -3,7 +3,7 @@ const theme = {
     // accentGreen: '#62CABE',
     accentGreen: '#0CD4AF',
     accentPurple: '#6876CB',
-    secondaryPurple: 'black',
+    secondaryPurple: '#5B66AC',
     hoverGreen: '',
     hoverPurple: '#5969c6',
     lightGreen: '#e2fdf8',
